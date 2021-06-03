6e4077988809ffca8a16aa581f1d53138fcf7731
ksfallon <fallon.katelin@gmail.com>
2021-06-03 18:34:30 -0400

    creating my constructor constants to connect the files

--- index.js
+++ index.js
@@ -1,89 +1,25 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path')
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-// Manager questions are:
-const managerQuestions = [
-    {
-        type: "input",
-        name: "firstName",
-        message: "Please enter team Manager's first name:"
-    },
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(fileName), data)
+}
 
-    {
-        type: "input",
-        name: "employeeID",
-        message: "Please enter team Manager's Employee ID:"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter team Manager's email address:"
-    },
-    {
-        type: "input",
-        name: "office",
-        message: "Please enter team Manager's office number:"
-    },
-]
-
-// now creating a variable that gives three choices and based on that choice i am given a different list of questions
-    // { "Would you like to add another team member? Y or N "}
-    // {
-    //     type: "list",
-    //     name: "job",
-    //     message: "Please select team member's job title:",
-    //     choices: ["Manager", "Engineer", "Intern"]
-    // },
-    
-    // if they choose Engineer: name, ID, email, and github usuername
-    const engineerQuestions = [
-        {
-            type: "input",
-            name: "firstName",
-            message: "Please enter Engineers's first name:"
-        },
-    
-        {
-            type: "input",
-            name: "employeeID",
-            message: "Please enter Engineers's Employee ID:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter Engineers's email address:"
-        },
-        {
-            type: "input",
-            name: "office",
-            message: "Please enter Engineers's GitHub username:"
-        },
-    ]
-    // if they choose Intern: name, ID, email, and school
-    const internQuestions = [
-        {
-            type: "input",
-            name: "firstName",
-            message: "Please enter Intern's first name:"
-        },
-    
-        {
-            type: "input",
-            name: "employeeID",
-            message: "Please enter Intern's Employee ID:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter Engineers's email address:"
-        },
-        {
-            type: "input",
-            name: "office",
-            message: "Please enter InternIntern's school name:"
-        },
-    ]
-
-    // then you return to "Add more team members?"
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+.prompt(questions)
+.then(answers =>  {
+    writeToFile ('index.html', generateMarkdown({
+        // uses spread operator to concat answers hash
+        ...answers
+    }))
+    console.log("You've successfully created your team page!")
+})
+}
 
