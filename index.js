e67dfe4ebb3d24aa54244545fd75df37b776a6e3
ksfallon <fallon.katelin@gmail.com>
2021-06-03 22:48:12 -0400

    working on code

--- index.js
+++ index.js
@@ -23,3 +23,58 @@ function init() {
 })
 }
 
+// const employeeQuestions = [
+//     {
+//         type: "input",
+//         name: "firstName",
+//         message: "What is your employee's first name:"
+//     },
+
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is your  employee's ID:"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your  employee's email address:"
+//     },
+//     {
+//         type: "list",
+//         name: "job",
+//         message: "Which type of team member would you like to add?",
+//         choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+//     },
+
+// ]
+
+// const engineerQuestions = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is your engineer's name:"
+//     },
+
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is your engineer's ID:"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your engineer's email address:"
+//     },
+//     {
+//         type: "input",
+//         name: "github",
+//         message: "What is your engineer's GitHub username:"
+//     },
+//     {
+//         type: "list",
+//         name: "job",
+//         message: "Which type of team member would you like to add?",
+//         choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+//     },
+// ]
