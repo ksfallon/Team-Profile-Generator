900e009932d3d6cc3a21990f2a254a77c1b6e803
ksfallon <fallon.katelin@gmail.com>
2021-06-06 16:10:02 -0400

    fixed my inquirer, and jest packages, also fixed my Employee.test.js so the tests all pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,15 +3,15 @@ const Employee = require("../lib/Employee");
 describe("Employee class includes all methods", () => {
     const newEmployee = new Employee("Pam", 2, "pam@aol.com")
     it("The getName() should return newEmployee.name", () => {
-    expect(getName()).toBe("Pam");
+        expect(newEmployee.getName()).toBe("Pam");
     })
     it("The getId() should return newEmployee.id", () => {
-        expect(getName()).toBe(2);
+        expect(newEmployee.getId()).toBe(2);
     })
     it("The getEmail() should return newEmployee.email", () => {
-        expect(getName()).toBe("pam@aol.com");
+        expect(newEmployee.getEmail()).toBe("pam@aol.com");
     })
     it("The getRole() should return 'Employee'", () => {
-        expect(getRole()).toBe("Employee");
+        expect(newEmployee.getRole()).toBe("Employee");
     })
 })
